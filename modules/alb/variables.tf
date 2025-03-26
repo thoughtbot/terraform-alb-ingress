d3e5274c7dfd44f2738aa6b3273e626f98894c58
Olamide <Olamide@thoughtbot.com>
2025-03-26 11:35:39 +0100

    Enable access and connection logs for AWS ALB module

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -3,9 +3,32 @@ variable "description" {
   type        = string
 }
 
+variable "enable_access_logs" {
+  type        = bool
+  default     = false
+  description = "Enable or disable ALB access logs. If set to true, logs will be stored in an S3 bucket."
+}
+
+variable "enable_connection_logs" {
+  type        = bool
+  default     = false
+  description = "Enable or disable ALB connection logs. If set to true, logs will be stored in an S3 bucket."
+}
+
 variable "name" {
   description = "Name for this load balancer"
   type        = string
+}
+
+variable "security_group_name" {
+  type        = string
+  description = "Name for the load balancer security group; defaults to name"
+  default     = null
+}
+variable "s3_bucket_name" {
+  type        = string
+  default     = ""
+  description = "Optional S3 bucket name for storing ALB access logs. If not provided, a new bucket will be created."
 }
 
 variable "security_group_name" {
